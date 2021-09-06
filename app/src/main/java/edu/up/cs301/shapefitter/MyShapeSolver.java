9599082209360dc75efe7281eec5f52f624205a8
alishee <sheehan24@up.edu>
2021-09-05 23:44:49 -0700

    Added shape search functionality for original orientation

--- app/src/main/java/edu/up/cs301/shapefitter/MyShapeSolver.java
+++ app/src/main/java/edu/up/cs301/shapefitter/MyShapeSolver.java
@@ -1,9 +1,11 @@
 package edu.up.cs301.shapefitter;
 
+import java.util.Arrays;
+
 /**
  * Solver: finds fit for a shape; completed solution by Vegdahl.
  *
- * @author **** put your name here ****
+ * @author Alicia Sheehan
  * @version **** put completion date here ****
  */
 public class MyShapeSolver extends ShapeSolver {
@@ -26,11 +28,91 @@ public class MyShapeSolver extends ShapeSolver {
      * undisplay can be made.
      */
     public void solve() {
+        int worldHeight = world.length;
+        int worldWidth = world[0].length;
+        int shapeHeight = shape.length;
+        int shapeWidth = shape[0].length;
+
+        boolean[][] worldSegment = new boolean[shapeHeight][shapeWidth];
+
+        int i;
+        int j;
+        int ii;
+        int jj;
+
+
+
+        boolean shapeFound = false;
+        if (shapeHeight < worldHeight && shapeWidth < worldWidth) {
+
+
+
+            for (i=0; i < worldHeight - shapeHeight + 1; i++) {
+                for ( j = 0; j < worldWidth - shapeWidth + 1; j++) {
+                    //Maybe use stream to copy a smaller array into worldSegment??
+
+
+                    for (ii = 0; ii < shapeHeight; ii++) {
+                        for (jj = 0; jj < shapeWidth; jj++) {
+                            worldSegment[ii][jj] = world[i+ii][j+jj];
+                        }
+                    }
+                    //compare shape and worldSegment
+                    if(Arrays.deepEquals(worldSegment,shape)) {
+                        display(i, j, Orientation.ROTATE_NONE);
+                        shapeFound = true;
+                    }
+                    if(!shapeFound) {
+                        undisplay();
+                    }
+                    //Return boolean or int saying whether equal
+                    //use i and j for display if equal
+
+                }
+            }
+
+
+
+        }
+
+    }
+
+    /*
+    GOAL: OPTIMIZE solve()
+ For example, if the shape is a square, no orientations
+other than the unrotated one should be tested, since a square is identical to itself no matter
+how it is rotated (in the 90-degree sense) or reflected; a vertical line would require that
+only two orientations be tested; a “T” shape would require only four orientations to be
+tested
+     */
+
+
 
         // ****dummied up****
         // always "finds" a solution at row 3, column 4, with a 90-degree clockwise orientation
-        display(3, 4, Orientation.ROTATE_CLOCKWISE);
+        //display(3, 4, Orientation.ROTATE_CLOCKWISE);
+
+    /**
+     * Creates a new array of the array shape to the specified orientation.
+     * @param orient the desired orientation of the new shape
+     * @return a new 2-Dimensional boolean array containing the transformed shape
+     */
+    public boolean[][] changeOrientation(Orientation orient) {
+        //use switch statement followed by for loop to transform shape into new array
+        return null;
     }
+
+
+    /**
+     *
+     * @return
+     */
+    public Orientation[] discreteOrientations() {
+        //
+        return null;
+
+    }
+
 
     /**
      * Checks if the shape is well-formed: has at least one square, and has all squares connected.
